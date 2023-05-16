6b60cfc58100f7adf64da681d531d6a44ffbf748
djzib0 <piotrsabatura@gmail.com>
2023-05-16 21:05:15 +0200

    added client view and filter for companies

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -10,7 +10,12 @@ import './companies.css'
 function Companies() {
     const {allCompaniesData} = useDatabaseHook()
 
-    let companiesArr = allCompaniesData && allCompaniesData.map(item => {
+    const [filterForm, setFilterForm] = useState({
+        filterByCompanyName: "",
+        filterByCity: "",
+    })
+
+    let companiesArr = allCompaniesData && filterData().map(item => {
         return (
             <div key={item[0]} className='container company-container'>
                  <Link to={`/company/${item[0]}`}><p>{item[1].companyName}</p></Link>
@@ -19,9 +24,61 @@ function Companies() {
         )
     })
 
+
+    let CITIES = allCompaniesData && allCompaniesData.map(item => {
+        let city = item[1].companyAddressCity
+        return (
+            <option key={item[0]} value={city}>{city}</option>
+        )
+    })
+  
+    function filterData() {
+        // for each filter input create a new array with filter method
+        let filteredArr = allCompaniesData.filter(item => {
+            return (
+                item[1].companyName.toLowerCase().includes(filterForm.filterByCompanyName.toLowerCase()) &&
+                item[1].companyAddressCity.toLowerCase().includes(filterForm.filterByCity.toLowerCase())
+            )
+        })
+
+
+
+        return filteredArr
+    }
+
+
+    function handleChange(e) {
+        const {name, value} = e.target
+        setFilterForm(prevFormData => {
+            return {
+                ...prevFormData,
+                [name]: value
+            }
+        })
+    }
+
+    console.log(filterForm.filterByCity)
     
     return (
         <div>
+            <form>
+                <input type='text'
+                       className=''
+                       name='filterByCompanyName'
+                       value={filterForm.filterByCompanyName}
+                       placeholder='Search by company name'
+                       onChange={handleChange}
+                       />
+                <select className=''
+                        name='filterByCity'
+                        value={filterForm.filterByCity}
+                        onChange={handleChange}
+                >
+                <option value=''>-</option>
+                {CITIES}
+                </select>
+                
+            </form>
             <Link to="/add-company" className='link-btn'>Add Company</Link>
             {companiesArr}
         </div>
