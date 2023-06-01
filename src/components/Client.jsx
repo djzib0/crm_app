62c8275b88866477a2c103703d831161b7fb387a
djzib0 <piotrsabatura@gmail.com>
2023-06-01 19:01:22 +0200

    developing client details view

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Client() {
   return (
-    <div>Client</div>
+    <div className='client__container'>
+      <div className='client__details-container'>
+        <div className='client__avatar-container'>
+          tutaj jakiś img w kółeczku
+        </div>
+        <div className='client__info-container'>
+          Tutaj dane od klienta
+        </div>
+      </div>
+    </div>
   )
 }
 
