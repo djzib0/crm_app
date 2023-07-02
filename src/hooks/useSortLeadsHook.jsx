10acfcd4c218391a3c3514fa723206ffb328c24e
djzib0 <piotrsabatura@gmail.com>
2023-07-02 14:27:01 +0200

    added corect sorting of value, client and company

--- src/hooks/useSortLeadsHook.jsx
+++ src/hooks/useSortLeadsHook.jsx
@@ -17,30 +17,10 @@ function useSortLeadsHook() {
         sortByPotential: false,
     })
 
-
-    // function toggleSort(propertyName) {
-    //     // check object has property named propertyName
-    //     console.log(sortData)
-    //     for (const key of Object.keys(sortData)) {
-    //         console.log(key)
-    //     }
-    //     if (propertyName in sortData) {
-    //         // if yes, set property value to reverse false or true
-    //         setSortData(prevData => {
-    //             return {
-    //                 ...prevData,
-    //                 [propertyName]: !prevData[propertyName]
-    //             }
-    //         })
-    //     }
-    //     console.log(sortData.sortByTitle)
-    // }
-
     function toggleSort(sortPropertyName) {
         Object.keys(sortData).forEach(key => {
             setSortData(prevData => {
                 if (key === sortPropertyName) {
-
                     return {
                         ...prevData,
                         [key]: !prevData[sortPropertyName]
@@ -56,47 +36,67 @@ function useSortLeadsHook() {
         })
     }
 
-
     function sortList(arr, sortPropertyName, propertyName) {
-        const sortedLeads = arr.sort((a, b) => {
-            try {
-                if (sortData[sortPropertyName]) {
-                    if (a[1][propertyName].toLowerCase() > b[1][propertyName].toLowerCase()) {
-                        return 1
-                    } else {
-                        return -1
-                    }
-                } else {
-                    if (a[1][propertyName].toLowerCase() < b[1][propertyName].toLowerCase()) {
-                        return 1
-                    } else {
-                        return -1
-                    }
-                } 
-            } 
-            catch {
-                if (sortData[sortPropertyName]) {
-                    if (a[1][propertyName] > b[1][propertyName]) {
-                        return 1
-                    } else {
-                        return -1
-                    }
-                } else {
-                    if (a[1][propertyName] < b[1][propertyName]) {
-                        return 1
-                    } else {
-                        return -1
-                    }
-                } 
+        console.log(arr, "arr in sortlist")
+        console.log(sortPropertyName, "sortproperty name")
+        console.log(propertyName, "PropertyName")
+        // map new array with projectValue converted to a Number
+        const arrWithNumbers = arr.map(item => {
+            return {
+                ...item,
+                1: {...item[1], projectValue: Number(item[1].projectValue)}
             }
-            
         })
-        return sortedLeads
+
+        if (propertyName != "projectValue") {
+            const sortedLeads = arrWithNumbers.sort((a, b) => {
+                try {
+                    if (sortData[sortPropertyName]) {
+                        console.log
+                        if (a["1"][propertyName].toLowerCase() > b["1"][propertyName].toLowerCase()) {
+                            return 1
+                        } else {
+                            return -1
+                        }
+                    } else {
+                        if (a["1"][propertyName].toLowerCase() < b["1"][propertyName].toLowerCase()) {
+                            return 1
+                        } else {
+                            return -1
+                        }
+                    } 
+                } 
+                // handling properties which returns true or false
+                // on which you can't use toUpperCase()
+                catch {
+                    if (sortData[sortPropertyName]) {
+                        if (a[1][propertyName] > b[1][propertyName]) {
+                            return 1
+                        } else {
+                            return -1
+                        }
+                    } else {
+                        if (a[1][propertyName] < b[1][propertyName]) {
+                            return 1
+                        } else {
+                            return -1
+                        }
+                    } 
+                }
+            })
+            return sortedLeads
+        // if propertyName === projectValue then sort as numbers
+        } else {
+            let sortedLeads = {}
+            if (sortData[sortPropertyName]) {
+                sortedLeads = arrWithNumbers.sort((a, b) => (a["1"]["projectValue"] - b["1"]["projectValue"]))
+            } else {
+                sortedLeads = arrWithNumbers.sort((a, b) => (b["1"]["projectValue"] - a["1"]["projectValue"]))
+            }
+            return sortedLeads
+        }
     }
 
-
-
-    
     return {
         sortData,
         toggleSort,
